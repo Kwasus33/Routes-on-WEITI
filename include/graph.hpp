f2fdb0a230e4421d75b2c5b3a0f05bf624c015ce
Kajetan Witkowski <01187301@pw.edu.pl>
2024-05-11 18:12:46 +0200

    added getRoute method

--- include/graph.hpp
+++ include/graph.hpp
@@ -9,6 +9,7 @@ class Graph
     void addNode(Node new_node);
     std::vector<Node>& getNodes();
     void findRoutes(int nodeID);
+    std::vector<int> getRoute(int start, int end);
     private:
     std::vector<Node> nodes;
     std::vector<bool> hasRoutes;
