2e4cc04bd66682c9ec8508d99bb921718b278d0d
Kajetan Witkowski <01187301@pw.edu.pl>
2024-05-14 09:16:07 +0200

    added classroom usage

--- include/node.hpp
+++ include/node.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include "classroom.hpp"
 
 class Node{
     public:
@@ -8,11 +9,15 @@ class Node{
     int getID() const;
     std::vector<int>& getDistances();
     std::vector<int>& getNextNodes();
+    std::vector<Classroom>& getClassrooms();
     void setDistance(int nodeID, int distance);
     void addRoute(int ID, int distance, int nextNode);
     void setNextNode(int toNode, int nextNode);
+    void addClassroom(Classroom& new_classroom);
+    bool findClassroom(std::string& class_name);
     private:
     int ID;
     std::vector<int> distances;
     std::vector<int> nextNodes;
+    std::vector<Classroom> classrooms;
 };
