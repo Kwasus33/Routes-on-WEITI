7d92c6dd315060b0d95ecac31ced0fae20dd215d
Jakub Kwaśniak <kubak04@outlook.com>
2024-05-14 17:18:07 +0200

    added json and csv file handling - json with bugs

---
+++ include/file_handle.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "node.hpp"
+#include <vector>
+#include <ostream>
+#include <istream>
+#include <fstream>
+#include "../build/_deps/jsonrapid-src/include/rapidjson/document.h"
+#include "../build/_deps/jsonrapid-src/include/rapidjson/filereadstream.h"
+
+
+class csvReader {
+    public:
+        csvReader();
+        csvReader(const std::string& path);
+        void LoadFromFile(const std::string path);
+        std::vector<Node> getData() const;
+        //friend std::istream& operator >>(std::istream &is, const csvReader &csv);
+    private:
+        std::string path;
+        std::vector<Node> data;
+        Node addNode(std::string& line);
+        void isReadPathValid(const std::ifstream& fp) const; // dodane jako metody public nie private bo operatory z nich będą korzystać
+        //void isWritePathValid(const std::ofstream& fp) const;
+};
+
+class jsonReader {
+    public:
+        jsonReader();
+        jsonReader(const std::string& path);
+        void LoadFromFile(const std::string path);
+        std::vector<Node> getData() const;
+        //sfriend std::istream& operator >>(std::istream &is, const jsonReader &json);
+    private:
+        std::string path;
+        std::vector<Node> data;
+        std::vector<Node> addNodes(Document document);
+        void isReadPathValid(const std::ifstream& fp) const; // dodane jako metody public nie private bo operatory z nich będą korzystać
+};
---
+++ src/file_handle.cpp
@@ -0,0 +1,142 @@
+#include "../include/file_handle.hpp"
+#include "../build/_deps/jsonrapid-src/include/rapidjson/document.h"
+#include <sstream>
+#include <string>
+#include <iostream>
+
+using namespace rapidjson;
+
+
+csvReader::csvReader(): path(), data() {};
+
+csvReader::csvReader(const std::string &path): path(path) {};
+
+std::vector<Node> csvReader::getData() const {
+    return data;
+};
+
+void csvReader::LoadFromFile(const std::string path) {
+    std::ifstream fp_in(path); //opens file
+    isReadPathValid(fp_in);
+    std::string line;
+    while(!fp_in.eof()) {
+        fp_in >> line;
+        if(!line.empty())
+            addNode(line);
+    }
+    fp_in.close();
+}
+
+Node csvReader::addNode(std::string& line) {
+    std::vector<std::string> values;
+    std::vector<int> distances;
+
+    size_t start = 0;
+    size_t end = line.find(',');
+
+    try {
+        // while (end != std::string::npos) {
+        while (end != '\n') {
+            values.push_back(line.substr(start, end - start));
+            start = end + 1;
+            end = line.find(',', start);
+        }
+        values.push_back(line.substr(start));
+
+        int id = stoi(values.at(0));
+        
+        int i = 1;
+        while (i < size(values)) {  
+            distances.push_back(stoi(values.at(i)));      
+            ++i;
+        }
+
+        return Node(id, distances);
+    }
+    catch (std::invalid_argument &error) {
+        return;
+    }
+    catch (std::out_of_range &error) {  // for .at() method
+        return;
+    }
+    // if error ocurrs the data line is skipped
+};
+
+void csvReader::isReadPathValid(const std::ifstream &fp) const{
+    if (!fp.is_open())
+        std::cerr << "Failed to open file" << std::endl;
+}
+
+//////      rapid json jsonReader    ///////
+
+jsonReader::jsonReader(): path(), data() {};
+
+jsonReader::jsonReader(const std::string &path): path(path) {};
+
+std::vector<Node> jsonReader::getData() const {
+    return data;
+};
+
+void jsonReader::LoadFromFile(const std::string path) {
+
+    std::ifstream fp_in(path); //opens file
+    isReadPathValid(fp_in);
+
+    // Read the JSON content into a string
+    std::string jsonContent((std::istreambuf_iterator<char>(fp_in)), (std::istreambuf_iterator<char>()));
+
+    // Close the file stream
+    fp_in.close();
+
+    // Parse the JSON content
+    Document document;
+
+    if (document.Parse(jsonContent.c_str()).HasParseError()) {
+        std::cerr << "Error parsing the JSON." << std::endl;
+        return;
+    }
+
+    // Access JSON array elements
+    if (!document.IsArray()) {
+        std::cerr << "JSON content is not an array." << std::endl;
+        return;
+    }
+
+    addNodes(document);
+    return;
+
+}
+// nie czyta pliku document.h z biblioteki rapidjson
+
+std::vector<Node> jsonReader::addNodes(Document document) {
+    
+    std::vector<Node> Nodes;
+    std::vector<int> distancesVect;
+    int id;
+    
+    // Iterate over each object in the array
+    for (SizeType i = 0; i < document.Size(); i++) {
+        const Value& obj = document[i];
+
+        // Check if the object has an "id" field
+        if (obj.HasMember("id") && obj["id"].IsInt()) {
+            id = obj["id"].GetInt();
+        }
+
+        // Check if the object has a "distances" field
+        if (obj.HasMember("distances") && obj["distances"].IsArray()) {
+            const Value& distances = obj["distances"];
+            for (SizeType j = 0; j < distances.Size(); j++)
+                if (distances[j].IsInt())
+                    distancesVect.push_back(distances[j].GetInt());
+        }
+
+        Nodes.push_back(Node(id, distancesVect));        
+    }
+    return Nodes;
+}
+
+void jsonReader::isReadPathValid(const std::ifstream &fp) const{
+    if (!fp.is_open())
+        std::cerr << "Failed to open file" << std::endl;
+}
