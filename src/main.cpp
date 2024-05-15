449a1ba8f4a107bc4cda49fdf40a7fa8dfe8fdd5
Jakub Kwaśniak <kubak04@outlook.com>
2024-05-15 23:01:25 +0200

    need to add argparser

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,30 @@
 
 int main(int argc, char *argv[])
 {
+    // std::vector<std::string> arguments;
+
+    // for (int i = 1; i < argc; ++i) {
+    //     std::string arg(argv[i]);
+
+    //     if (arg == "-h" || arg == "--help") {
+    //         std::cout << "Usage: " << argv[0] << " [options]\n";
+    //         std::cout << "Options:\n";
+    //         std::cout << "  -h, --help  Show this help message\n";
+    //     }
+    //     else if (arg == "-f" || arg == "--find") {
+    //         // call findRoute() func
+    //     }
+    //     else if (arg == "-" || arg == "--find"){
+    //         arguments.push_back(arg);
+    //     }
+    // }
+
+    // // Process custom arguments
+    // for (const std::string& arg : arguments) {
+    //     // Add your custom argument processing logic here
+    //     std::cout << "Custom argument: " << arg << "\n";
+    // }
+
     jsonReader fh("../test/test.json");
     Graph gr = fh.addNodes();
 
