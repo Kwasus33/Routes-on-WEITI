43028fb3b027879910db090902661f24d3088718
Kajetan Witkowski <01187301@pw.edu.pl>
2024-05-15 16:52:50 +0200

    example

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,19 @@
 
 int main(int argc, char *argv[])
 {
-    jsonReader fh("../test/eiti.json");
+    jsonReader fh("../test/test.json");
     Graph gr = fh.addNodes(fh.LoadFromFile());
-    for (Node node : gr.getNodes())
+    std::string str1 = "01";
+    std::string str2 = "52";
+    int class1 = gr.findClassroom(str1);
+    int class2 = gr.findClassroom(str2);
+    gr.findRoutes(class1);
+    std::vector<int> result = gr.getRoute(class1, class2);
+    for (int i : result)
     {
-        std::cout << node.getClassrooms()[0].getName();
+        std::cout << i << " ";
     }
+    std::cout << std::endl;
     std::cout << "start" << std::endl;
     ProgramManager program(800, 600);
 
