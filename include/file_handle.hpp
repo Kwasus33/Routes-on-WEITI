d11ba73717ad3ee539c7958dbd3c05b7ec77c675
Jakub Kwaśniak <kubak04@outlook.com>
2024-05-20 23:22:42 +0200

    fixed inheritance issue

--- include/file_handle.hpp
+++ include/file_handle.hpp
@@ -15,7 +15,7 @@ class FileReader {
         FileReader(const std::string& path);
         virtual ~FileReader();
         virtual Graph ReadDataIntoGraph() = 0;
-    private:
+    protected:
         std::string path;
 };
 
@@ -25,7 +25,6 @@ class jsonReader: public FileReader {
         jsonReader(const std::string& path);
         Graph ReadDataIntoGraph();
     private:
-        std::string path;
         json LoadFromFile();
         void isReadPathValid(const std::ifstream& fp) const;
 };
@@ -36,7 +35,6 @@ class csvReader: public FileReader {
         csvReader(const std::string& path);
         Graph ReadDataIntoGraph();
     private:
-        std::string path;
         Node addNode(std::string& line1, std::string& line2, std::string& line3);
         void isReadPathValid(const std::ifstream& fp) const;
 };
