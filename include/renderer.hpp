8ec7aa57f3325d70c8a765e547a4427d552f19cf
mopi <poterahubert@gmail.com>
2024-05-27 18:11:57 +0200

    window creation

--- include/renderer.hpp
+++ include/renderer.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
+#include "SDL.h"
+
 class Renderer
 {
     int windowWitdth, windowHeight;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
 public:
     Renderer(int windowWidth, int windoHeight);
     void Render();
--- src/renderer.cpp
+++ src/renderer.cpp
@@ -8,5 +8,24 @@ Renderer::Renderer(int windowWidth, int windowHeight)
 
 void Renderer::Render()
 {
-    
+    // Setup window
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
+        return;
+    }
+    window = SDL_CreateWindow("Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
+    if (!window)
+    {
+        std::cerr << "Window creation failed: " << SDL_GetError() << std::endl;
+        return;
+    }
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer)
+    {
+        std::cerr << "Renderer creation failed: " << SDL_GetError() << std::endl;
+        return;
+    }
+    SDL_SetRenderDrawColor(renderer, 255, 255, 200, 255);
+    SDL_RenderClear(renderer);
 }
