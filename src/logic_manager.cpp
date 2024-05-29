ac28c0dbb66f6e94ce03f5718028cfeb2a7fe5b5
mopi <poterahubert@gmail.com>
2024-05-29 15:32:58 +0200

    change floor function

--- src/logic_manager.cpp
+++ src/logic_manager.cpp
@@ -1,10 +1,25 @@
 #include "logic_manager.hpp"
 
 #include "file_handler.hpp"
+#include "config.hpp"
 
 LogicManager::LogicManager()
     : currentFloor(0)
 {
     jsonReader fh("../test/test.json");
     graph = fh.ReadDataIntoGraph();
+}
+
+void LogicManager::UpdatePath()
+{
+
+}
+
+void LogicManager::ChangeFloor(int change)
+{
+    currentFloor += change;
+    if (currentFloor < config::MIN_FLOOR || currentFloor > config::MAX_FLOOR)
+    {
+        currentFloor -= change;
+    }
 }
