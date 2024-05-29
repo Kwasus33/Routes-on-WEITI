1d39d9524a2edcd440b33122a74062d57c88126a
Jakub Kwaśniak <kubak04@outlook.com>
2024-05-29 14:47:10 +0200

    changed file_handlers so all floor can be stored in separated files to which paths are stored in vector

--- include/file_handler.hpp
+++ include/file_handler.hpp
@@ -12,29 +12,33 @@ using json = nlohmann::json;
 class FileReader {
     public:
         FileReader();
-        FileReader(const std::string& path);
+        FileReader(const std::vector<std::string>& pathsVec);
         virtual ~FileReader();
         virtual Graph ReadDataIntoGraph() = 0;
     protected:
-        std::string path;
+        std::vector<std::string> pathsVec;
 };
 
 class jsonReader: public FileReader {
     public:
         jsonReader();
-        jsonReader(const std::string& path);
+        jsonReader(const std::vector<std::string>& pathsVec);
         Graph ReadDataIntoGraph();
     private:
-        json LoadFromFile();
+        json LoadFromFile(const std::string path);
         void isReadPathValid(const std::ifstream& fp) const;
+        Node createNode(const json& obj);
+        void createClassrooms(const json& obj, Node& node);
 };
 
 class csvReader: public FileReader {
     public:
         csvReader();
-        csvReader(const std::string& path);
+        csvReader(const std::vector<std::string>& pathsVec);
         Graph ReadDataIntoGraph();
     private:
         Node addNode(std::string& line1, std::string& line2, std::string& line3);
         void isReadPathValid(const std::ifstream& fp) const;
+        Node createNode(const std::string& line1, const std::string& line2);
+        void createClassrooms(const std::string& line3, Node& node); 
 };
