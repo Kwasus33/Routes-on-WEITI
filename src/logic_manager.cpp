6de86e303648e48e79bd827ec3e72d5e0b09b043
mopi <poterahubert@gmail.com>
2024-06-01 13:54:10 +0200

    merge

--- src/logic_manager.cpp
+++ src/logic_manager.cpp
@@ -13,22 +13,6 @@ LogicManager::LogicManager(ResourceManager* resourceManager)
 
 void LogicManager::UpdatePath(std::string& start, std::string& end)
 {
-<<<<<<< HEAD
-    int startID = graph.findClassroom(start);
-    if(startID == -1)
-    {
-        std::cout << "Couldn't find class: " << start << std::endl;
-        return;
-    }
-    int endID = graph.findClassroom(end);
-    if(endID == -1)
-    {
-        std::cout << "Couldn't find class: " << end << std::endl;
-        return;
-    }
-    
-    graph.findRoutes(startID);
-=======
     // Tries to find classrooms with given names
     std::vector<int> startSearches = graph.findClassrooms(start);
     std::vector<int> endSearches = graph.findClassrooms(end);
@@ -42,7 +26,6 @@ void LogicManager::UpdatePath(std::string& start, std::string& end)
     std::sort(endSearches.begin(), endSearches.end(), [&startID, this](int nodeOne, int nodeTwo)
               { return graph.getDistance(startID, nodeOne) < graph.getDistance(startID, nodeTwo); });
     int endID = endSearches[0];
->>>>>>> 2a0549c98ada99b98da7e09a7cd14dd29e505d7e
     std::vector<int> path = graph.getRoute(startID, endID);
     resourceManager->GetPath().SetFromGraph(&graph, path);
 }
