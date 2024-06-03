6f0f56c10a9b131fb75066aca3808ef1aaae793b
mopi <poterahubert@gmail.com>
2024-06-03 12:29:08 +0200

    add consol interface header

---
+++ include/console_interface.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "graph.hpp"
+#include "input_manager.hpp"
+
+enum class Action
+{
+    QUIT, SHOW_PATH, SHOW_DESCRIPTION
+};
+
+class ConsoleInterface
+{
+    const InputManager* inputManager;
+
+public:
+    ConsoleInterface(const InputManager* inputManager)
+
+    Action GetNextAction() const;
+    void SetNewPath() const;
+    void ShowDescription() const;
+};
