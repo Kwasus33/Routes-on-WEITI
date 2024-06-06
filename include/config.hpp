4b2d2bc70a391c8be2c2c91c22ec33b8e08cdc9b
Kajetan Witkowski <01187301@pw.edu.pl>
2024-06-06 12:56:18 +0200

    fixed all bmp, added descriptions for classerooms

--- include/config.hpp
+++ include/config.hpp
@@ -4,7 +4,7 @@ namespace config
 {
     constexpr const char* TEXTURE_PATH_PIWNICA = "../assets/piwnica_ids.bmp";
     constexpr const char* TEXTURE_PATH_PARTER = "../assets/parter_ids.bmp";
-    constexpr const char* TEXTURE_PATH_PIERWSZE = "../assets/pierwsze.bmp";
+    constexpr const char* TEXTURE_PATH_PIERWSZE = "../assets/pierwsze_ids.bmp";
 
     constexpr int MIN_FLOOR = -1;
     constexpr int MAX_FLOOR = 1;
