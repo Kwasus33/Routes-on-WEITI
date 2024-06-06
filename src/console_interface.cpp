c20e8e4dcd9ec4eedd02849395ddbef084100606
Kajetan Witkowski <01187301@pw.edu.pl>
2024-06-06 11:04:07 +0200

    added nice formatting

--- src/console_interface.cpp
+++ src/console_interface.cpp
@@ -1,4 +1,6 @@
 #include "console_interface.hpp"
+#include <iostream>
+#include <iomanip>
 
 ConsoleInterface::ConsoleInterface(LogicManager *logicManager)
     : logicManager(logicManager) {}
@@ -7,12 +9,16 @@ Action ConsoleInterface::getNextAction() const
 {
     while (true)
     {
-        std::cout << "What would you like to do?" << std::endl;
-        std::cout << "p: Show a path between two classrooms." << std::endl;
-        std::cout << "d: Show the description of a classroom." << std::endl;
-        std::cout << "q: Quit." << std::endl
-                  << std::endl;
+        std::cout << std::setw(52) << std::setfill('=') << "" << std::endl;
 
+        std::cout << "What would you like to do?" << std::endl;
+        std::cout << "-> Show a path between two classrooms - ENTER p" << std::endl;
+        std::cout << "-> Show the description of a classroom - ENTER d" << std::endl;
+        std::cout << "-> Quit - ENTER q" << std::endl;
+
+        std::cout << std::setw(52) << std::setfill('=') << "" << std::endl;
+
+        std::cout << "-> ";
         std::string input;
         std::getline(std::cin, input);
 
@@ -26,11 +32,14 @@ Action ConsoleInterface::getNextAction() const
         }
         else if (input == "q")
         {
+            std::cout << std::setw(52) << std::setfill('=') << "" << std::endl;
+            std::cout << "Goodbye! System shutting down." << std::endl;
+            std::cout << std::setw(52) << std::setfill('=') << "" << std::endl;
             return Action::QUIT;
         }
-
-        std::cout << "Invalid input. Try again." << std::endl
-                  << std::endl;
+        std::cout << std::setw(52) << std::setfill('!') << "" << std::endl;
+        std::cout << "Invalid input. Try again." << std::endl;
+        std::cout << std::setw(52) << std::setfill('!') << "" << std::endl;
     }
 }
 
@@ -41,10 +50,15 @@ void ConsoleInterface::SetNewPath(std::string room1, std::string room2) const
         // Optionally skip input if rooms are already decided
         if (room1 == "" || room2 == "")
         {
-            std::cout << "Type in the name of the first classroom:" << std::endl;
+            std::cout << std::setw(52) << std::setfill('-') << "" << std::endl;
+            std::cout << "Type in the name of the first classroom" << std::endl;
+            std::cout << "-> ";
             std::getline(std::cin, room1);
+            std::cout << std::setw(52) << std::setfill('-') << "" << std::endl;
             std::cout << "Type in the name of the second classroom:" << std::endl;
+            std::cout << "-> ";
             std::getline(std::cin, room2);
+            std::cout << std::setw(52) << std::setfill('-') << "" << std::endl;
         }
 
         try
@@ -54,8 +68,9 @@ void ConsoleInterface::SetNewPath(std::string room1, std::string room2) const
         }
         catch (const std::invalid_argument &e)
         {
-            std::cout << "Invalid input. Try again." << std::endl
-                      << std::endl;
+            std::cout << std::setw(52) << std::setfill('!') << "" << std::endl;
+            std::cout << "Invalid classroom(s) provided. Try again" << std::endl;
+            std::cout << std::setw(52) << std::setfill('!') << "" << std::endl;
             room1 = "";
             room2 = "";
         }
@@ -69,20 +84,26 @@ void ConsoleInterface::ShowDescription(std::string room) const
         // Optionally skip input if room is already decided
         if (room == "")
         {
-            std::cout << "Type in the name of the classroom:" << std::endl;
-            std::cin >> room;
+            std::cout << std::setw(52) << std::setfill('-') << "" << std::endl;
+            std::cout << "Type in the name of the first classroom" << std::endl;
+            std::cout << "-> ";
+            std::getline(std::cin, room);
+            std::cout << std::setw(52) << std::setfill('-') << "" << std::endl;
         }
 
         try
         {
             std::string desc = logicManager->getDescription(room);
+            std::cout << std::setw(52) << std::setfill('*') << "" << std::endl;
             std::cout << desc << std::endl;
+            std::cout << std::setw(52) << std::setfill('*') << "" << std::endl;
             return;
         }
         catch (const std::invalid_argument &e)
         {
-            std::cout << "Invalid input. Try again." << std::endl
-                      << std::endl;
+            std::cout << std::setw(52) << std::setfill('!') << "" << std::endl;
+            std::cout << "Invalid classroom(s) provided. Try again" << std::endl;
+            std::cout << std::setw(52) << std::setfill('!') << "" << std::endl;
             room = "";
         }
     }
